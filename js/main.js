29efd1197bf7d0335fb4ee531236084554b92e15
Agamali <muhammad.agamalieff@yandex.ru>
2023-10-16 11:31:56 +0500

    Изменения

--- js/main.js
+++ js/main.js
@@ -1,45 +1,69 @@
-// Функция для генерации случайного числа в заданном диапазоне
+const authorNames = ['Иван', 'Анна', 'Петр', 'Мария', 'Алексей', 'Елена', 'Дмитрий', 'Ольга', 'Сергей', 'Наталья'];
+const comments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const MIN_AVATAR_NUMBER = 1;
+const MAX_AVATAR_NUMBER = 6;
+
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+
+const MIN_COMMENTS_COUNT = 0;
+const MAX_COMMENTS_COUNT = 30;
+
+const NUM_PHOTOS = 25; // Количество фотографий
+
+const photoDescriptions = [
+  'Красивый закат на пляже',
+  'Горизонтальное излучение солнца в лесу',
+  'Городской пейзаж вечером',
+  'Величественные горы в облаках',
+  'Парусная лодка на озере',
+  'Цветущие вишневые деревья',
+  // Добавьте здесь ещё описаний по вашему усмотрению
+];
+
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Список случайных имен для авторов комментариев
-const authorNames = ['Иван', 'Анна', 'Петр', 'Мария', 'Алексей', 'Елена', 'Дмитрий', 'Ольга', 'Сергей', 'Наталья'];
+function getRandomAvatarNumber() {
+  return getRandomNumber(MIN_AVATAR_NUMBER, MAX_AVATAR_NUMBER);
+}
 
-// Функция для генерации случайного имени автора комментария
 function getRandomAuthorName() {
   const randomIndex = getRandomNumber(0, authorNames.length - 1);
   return authorNames[randomIndex];
 }
 
-// Функция для генерации случайного текста комментария
-function generateRandomComment() {
-  const comments = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-  ];
+function getRandomPhotoDescription() {
+  const randomIndex = getRandomNumber(0, photoDescriptions.length - 1);
+  return photoDescriptions[randomIndex];
+}
 
+function generateRandomComment() {
   const randomIndex = getRandomNumber(0, comments.length - 1);
   const commentText = comments[randomIndex];
 
   return {
-    id: getRandomNumber(1, 1000), // Генерируем случайный id для комментария
-    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`, // Генерируем случайную аватарку
+    id: getRandomNumber(1, 1000),
+    avatar: `img/avatar-${getRandomAvatarNumber()}.svg`,
     message: commentText,
-    name: getRandomAuthorName(), // Генерируем случайное имя автора
+    name: getRandomAuthorName(),
   };
 }
 
-// Функция для создания объекта фотографии с комментариями
 function generatePhoto(id) {
   const url = `photos/${id}.jpg`;
-  const description = `Описание фотографии номер ${id}`;
-  const likes = getRandomNumber(15, 200);
-  const commentsCount = getRandomNumber(0, 30);
+  const description = getRandomPhotoDescription();
+  const likes = getRandomNumber(MIN_LIKES, MAX_LIKES);
+  const commentsCount = getRandomNumber(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT);
   const comments = [];
 
   for (let i = 0; i < commentsCount; i++) {
@@ -56,11 +80,10 @@ function generatePhoto(id) {
   };
 }
 
-// Функция для создания массива из 25 сгенерированных объектов
 function generatePhotoArray() {
   const photoArray = [];
 
-  for (let i = 1; i <= 25; i++) {
+  for (let i = 1; i <= NUM_PHOTOS; i++) {
     const photo = generatePhoto(i);
     photoArray.push(photo);
   }
@@ -68,6 +91,9 @@ function generatePhotoArray() {
   return photoArray;
 }
 
-// Создаем массив фотографий с комментариями
-const photos = generatePhotoArray();
+// Функция для получения массива сгенерированных объектов
+function getGeneratedPhotos() {
+  return generatePhotoArray();
+}
 
+const photos = getGeneratedPhotos();
